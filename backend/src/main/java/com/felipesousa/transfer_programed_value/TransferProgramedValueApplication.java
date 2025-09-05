29ab8e303cb46512e2db395890f1212ebce0d26d
Felipe Sousa <felipe.s.silva.ext@spms.min-saude.pt>
2025-09-05 19:22:41 -0300

    Start backend project

---
+++ backend/src/main/java/com/felipesousa/transfer_programed_value/TransferProgramedValueApplication.java
@@ -0,0 +1,13 @@
+package com.felipesousa.transfer_programed_value;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferProgramedValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferProgramedValueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/felipesousa/transfer_programed_value/TransferProgramedValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesousa.transfer_programed_value;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferProgramedValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
