a10eb11a9b80e19d1d838e2f8d88896a8df3a10d
Felipe Sousa <felipe.s.silva.ext@spms.min-saude.pt>
2025-09-06 10:38:49 -0300

    Feat: Configuration H2 for others web local and populate table transfer fee with values principal.

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (0, 0,   3.00, 2.50, NOW());
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (1, 10, 12.00, 0.00, NOW());
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (11, 20, 0.00, 8.20, NOW());
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (21, 30, 0.00, 6.90, NOW());
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (31, 40, 0.00, 4.70, NOW());
+INSERT INTO tb_transfer_fee (min_days, max_days, fixed_fee, percentage, created_at) VALUES (41, 50, 0.00, 1.70, NOW());
